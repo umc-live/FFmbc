d931e45f7eaaaf1f1ff54c9b66a73268f96378e0
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2011-02-08 00:34:34 -0800

    In mp4 muxer, allow pcm_s16be, supported by Quicktime.

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -631,17 +631,19 @@ static int mov_write_avid_tag(AVIOContext *pb, MOVTrack *track)
 
 static int mp4_get_codec_tag(AVFormatContext *s, MOVTrack *track)
 {
-    int tag = track->enc->codec_tag;
-
-    if (!ff_codec_get_tag(ff_mp4_obj_type, track->enc->codec_id))
-        return 0;
+    int tag = 0;
 
     if      (track->enc->codec_id == CODEC_ID_H264)      tag = MKTAG('a','v','c','1');
     else if (track->enc->codec_id == CODEC_ID_AC3)       tag = MKTAG('a','c','-','3');
+    else if (track->enc->codec_id == CODEC_ID_PCM_S16BE) tag = MKTAG('t','w','o','s');
     else if (track->enc->codec_id == CODEC_ID_DIRAC)     tag = MKTAG('d','r','a','c');
     else if (track->enc->codec_id == CODEC_ID_MOV_TEXT)  tag = MKTAG('t','x','3','g');
     else if (track->enc->codec_type == AVMEDIA_TYPE_VIDEO) tag = MKTAG('m','p','4','v');
     else if (track->enc->codec_type == AVMEDIA_TYPE_AUDIO) tag = MKTAG('m','p','4','a');
+
+    if ((tag == AV_RL32("mp4v") || tag == AV_RL32("mp4a")) &&
+        !ff_codec_get_tag(ff_mp4_obj_type, track->enc->codec_id))
+        return 0;
 
     return tag;
 }
