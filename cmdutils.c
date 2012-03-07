7e471ab9eba9bdc233b588ee14365c69b250eb23
Baptiste Coudurier <baptiste.coudurier@gmail.com>
2012-03-06 22:21:44 -0800

    ffmpeg: update copyright year

--- cmdutils.c
+++ cmdutils.c
@@ -54,7 +54,7 @@ AVFormatContext *avformat_opts;
 struct SwsContext *sws_opts;
 AVDictionary *format_opts, *codec_opts;
 
-static const int this_year = 2011;
+static const int this_year = 2012;
 
 void init_opts(void)
 {
